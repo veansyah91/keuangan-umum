bba655cf517522dc3cab2937479d585ff54ecce8
veansyah91 <49179821+veansyah91@users.noreply.github.com>
2024-05-10 08:41:16 +0700

    desktop 10 mei 2024

--- resources/js/Components/Desktop/ContentDesktop.jsx
+++ resources/js/Components/Desktop/ContentDesktop.jsx
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react'
 
-export default function ContentDesktop({children}) {
+function maxHeight() {
+  const screenHeight = window.screen.height;
+  const classHeight = `h-[${screenHeight-384}px]`;
+
+  return classHeight;
+}
+
+export default function ContentDesktop({children}) {  
   return (
     <div className='mt-5 pb-8'>
-        <div className={`overflow-x-auto h-[336px]`}>
+        <div className={`overflow-x-auto ${maxHeight()}`}>
             {children}
         </div>
     </div>
