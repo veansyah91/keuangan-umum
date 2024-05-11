5413df5e205ee2cdee1d28aa5f4fc10dd744d991
veansyah91 <49179821+veansyah91@users.noreply.github.com>
2024-05-11 22:33:26 +0700

    Update ContentDesktop.jsx

--- resources/js/Components/Desktop/ContentDesktop.jsx
+++ resources/js/Components/Desktop/ContentDesktop.jsx
@@ -6,8 +6,8 @@ export default function ContentDesktop({children}) {
   useLayoutEffect(() => {
     const screenHeight = window.screen.height;
     setHeightScreen(`h-[${(screenHeight-384).toString()}px]`)
-  }, [])
-
+  }, []);
+  
   return (
     <div className='mt-5 pb-8'>
         <div className={`overflow-x-auto ${heightScreen}`}>
