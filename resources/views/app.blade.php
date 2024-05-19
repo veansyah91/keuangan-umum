b656800b2993cbe8ddb887c54a6ac0c1d7c14c45
veansyah91 <veansyah91@gmail.com>
2024-05-19 13:53:50 +0700

    laptop 19 mei 2024

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Keuangan Umum') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Icon Title -->
+        <link rel="icon" href="/img/logo.ico"/>
 
         <!-- Scripts -->
         @routes
