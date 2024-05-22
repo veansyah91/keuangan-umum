3ba70e5dab2c5a2b2fcbfa11ed990083b805e640
veansyah91 <49179821+veansyah91@users.noreply.github.com>
2024-05-22 08:24:45 +0700

    desktop build 22 Mei 2024

---
+++ public/build/assets/ContainerDesktop-1f551737.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-71a08f08.js";function o({children:s}){return t.jsx("div",{className:"bg-white px-8 hidden sm:block",children:s})}export{o as C};
---
+++ public/build/assets/LogDesktop-c54d1bfc.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-71a08f08.js";import{d as a}from"./dayjs.min-b64be250.js";function i({logItem:t,className:s,...e}){return r.jsxs("tr",{className:s,...e,children:[r.jsx("td",{children:t.message}),r.jsx("td",{children:a(t.created_at).format("MMM DD, YYYY")})]})}export{i as default};
