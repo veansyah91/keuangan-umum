69e6686d394886d155dee68149dcc31a51d20400
veansyah91 <veansyah91@gmail.com>
2024-06-24 16:49:54 +0700

    laptop 24 juni 2024

---
+++ public/build/assets/ContainerDesktop-b0d6654e.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-6da0e1a2.js";function o({children:s}){return t.jsx("div",{className:"bg-white px-8 hidden sm:block",children:s})}export{o as C};
--- public/build/assets/LogDesktop-83992fba.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-e3d6b92a.js";import{d as a}from"./dayjs.min-bf118911.js";function i({logItem:t,className:s,...e}){return r.jsxs("tr",{className:s,...e,children:[r.jsx("td",{children:t.message}),r.jsx("td",{children:a(t.created_at).format("MMM DD, YYYY")})]})}export{i as default};
