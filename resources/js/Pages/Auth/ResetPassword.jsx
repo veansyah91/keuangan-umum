8f4e9791c3b300240927eea852d7615ed6aad648
veansyah91 <49179821+veansyah91@users.noreply.github.com>
2024-06-25 06:26:44 +0700

    Update ResetPassword.jsx

--- resources/js/Pages/Auth/ResetPassword.jsx
+++ resources/js/Pages/Auth/ResetPassword.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import GuestLayout from '@/Layouts/GuestLayout';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
