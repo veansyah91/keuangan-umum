52a3ce7f45fa341138f02134e578d7b533e333af
veansyah91 <49179821+veansyah91@users.noreply.github.com>
2024-06-25 06:29:05 +0700

    build

---
+++ public/build/assets/LogDesktop-86263dfc.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-9bc0302d.js";import{d as a}from"./dayjs.min-96472887.js";function i({logItem:t,className:s,...e}){return r.jsxs("tr",{className:s,...e,children:[r.jsx("td",{children:t.message}),r.jsx("td",{children:a(t.created_at).format("MMM DD, YYYY")})]})}export{i as default};
