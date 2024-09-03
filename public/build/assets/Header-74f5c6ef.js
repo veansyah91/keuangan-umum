33a8a450e1853d3a50174e4fe485d843fb7bf803
veansyah91 <49179821+veansyah91@users.noreply.github.com>
2024-09-03 09:29:56 +0700

    desktop 3 september 2024

--- public/build/assets/Header-74f5c6ef.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-2d0d68fd.js";function o({textColor:t="text-gray-800",children:e,className:r}){return s.jsx("h2",{className:`font-semibold text-xl leading-tight ${t} ${r}`,children:e})}export{o as H};
