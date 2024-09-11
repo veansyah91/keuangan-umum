af233f9e57a1336642276c1d93795fc201fc3bd1
veansyah91 <49179821+veansyah91@users.noreply.github.com>
2024-09-11 12:03:04 +0700

    Update StudentEntryPaymentDesktop.jsx

--- resources/js/Pages/StudentEntryPayment/Components/StudentEntryPaymentDesktop.jsx
+++ resources/js/Pages/StudentEntryPayment/Components/StudentEntryPaymentDesktop.jsx
@@ -1,7 +1,27 @@
+import formatNumber from '@/Utils/formatNumber';
+import dayjs from 'dayjs';
 import React from 'react'
 
-export default function StudentEntryPaymentDesktop() {
+export default function StudentEntryPaymentDesktop({
+  payment, className
+}) {
+  console.log(payment);
+  
   return (
-    <div>StudentEntryPaymentDesktop</div>
+    <tr className={className}>
+      <td>{dayjs(payment.date).locale('id').format('MMMM DD, YYYY')}</td>
+      <td>{ payment.no_ref }</td>
+      <td>
+        <div className='text-xs'>
+          No. Siswa: {payment.contact.student.no_ref}
+        </div>
+        <div>
+          {payment.contact.name}
+        </div>
+      </td>
+      <td>{payment.study_year}</td>
+      <td className='text-end'>IDR. { formatNumber(payment.value) }</td>
+      <td className=''></td>
+    </tr>
   )
 }
