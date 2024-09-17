2b200d584beccd0e112b735d3d6dc9b8dace67c9
veansyah91 <veansyah91@gmail.com>
2024-09-17 18:50:40 +0700

    Create StudentEntryReceivableController.php

---
+++ app/Http/Controllers/StudentEntryReceivableController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StudentEntryReceivable;
+use Illuminate\Http\Request;
+
+class StudentEntryReceivableController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StudentEntryReceivable $studentEntryReceivable)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(StudentEntryReceivable $studentEntryReceivable)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, StudentEntryReceivable $studentEntryReceivable)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StudentEntryReceivable $studentEntryReceivable)
+    {
+        //
+    }
+}
