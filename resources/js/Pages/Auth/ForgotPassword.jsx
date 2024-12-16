c1ee757d5c095961791274675f822dd67d061e6b
veansyah91 <49179821+veansyah91@users.noreply.github.com>
2024-12-16 21:26:59 +0700

    Update ForgotPassword.jsx

--- resources/js/Pages/Auth/ForgotPassword.jsx
+++ resources/js/Pages/Auth/ForgotPassword.jsx
@@ -24,7 +24,7 @@ export default function ForgotPassword({ status }) {
                 mengatur kembali password anda.
             </div>
 
-            {status && <div className='mb-4 font-medium text-sm text-green-600'>{status}</div>}
+            {status && <div className='mb-4 font-medium text-sm text-green-600'>Email permintaan perubahan password telah dikirim ke email anda, silakan cek pada email anda</div>}
 
             <form onSubmit={submit}>
                 <TextInput
