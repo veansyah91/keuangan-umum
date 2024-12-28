5807c664901ca3c9b29234341626fc8d2983515b
veansyah91 <49179821+veansyah91@users.noreply.github.com>
2024-12-28 08:33:49 +0700

    build 28 desember 2024

---
+++ public/build/assets/LogDesktop-467a806f.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-a7ce059d.js";import{d as a}from"./dayjs.min-4bdbfd9d.js";function i({logItem:t,className:s,...e}){return r.jsxs("tr",{className:s,...e,children:[r.jsx("td",{children:t.message}),r.jsx("td",{children:a(t.created_at).format("MMM DD, YYYY")})]})}export{i as default};
---
+++ public/build/assets/ProvinceDesktop-d986e3ef.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-a7ce059d.js";function n({province:r,className:e}){return t.jsx(t.Fragment,{children:t.jsxs("tr",{className:e,children:[t.jsx("td",{children:r.id}),t.jsx("td",{children:r.name}),t.jsx("td",{})]})})}export{n as default};
