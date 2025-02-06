ab7fd80390a45f83d74f713621075cd55dd9d41e
veansyah91 <49179821+veansyah91@users.noreply.github.com>
2025-02-06 22:21:58 +0700

    build 6 februari 2025

--- public/build/assets/PageDesktop-46921f18.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-ad4257d5.js";function s({page:r,className:t}){return e.jsx(e.Fragment,{children:e.jsx("tr",{className:t,children:e.jsx("td",{children:r.name})})})}export{s as default};
--- public/build/assets/close-provider-3c7c4627.js
+++
@@ -1,1 +0,0 @@
-import{r as t,R as a}from"./app-ad4257d5.js";let o=t.createContext(()=>{});function n({value:e,children:r}){return a.createElement(o.Provider,{value:e},r)}export{n as C};
