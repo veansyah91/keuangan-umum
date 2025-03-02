bd46b514e81ddb01de394dde73845ad1a48bc1e6
veansyah91 <veansyah91@gmail.com>
2025-03-02 14:27:09 +0700

    build 2 maret 2025

---
+++ public/build/assets/LogDesktop-78bec0f3.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-8a73958f.js";import{d as a}from"./dayjs.min-f50edeaa.js";function i({logItem:t,className:s,...e}){return r.jsxs("tr",{className:s,...e,children:[r.jsx("td",{children:t.message}),r.jsx("td",{children:a(t.created_at).format("MMM DD, YYYY")})]})}export{i as default};
--- public/build/assets/PageDesktop-971ba836.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-61cdbdaa.js";function s({page:r,className:t}){return e.jsx(e.Fragment,{children:e.jsx("tr",{className:t,children:e.jsx("td",{children:r.name})})})}export{s as default};
